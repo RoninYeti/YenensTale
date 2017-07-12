b0056e5f67bf00aacbd9f9242e1753c9bb8662cd
agleverone <alexgreglev@gmail.com>
2017-07-12 19:54:51 -0400

    commiting for push and pull

--- Assets/Custom Assets/Scripts/NormgBug.cs
+++ Assets/Custom Assets/Scripts/NormgBug.cs
@@ -7,19 +7,9 @@ namespace YenensTale {
 
         public float currentHealth, power, toughness;
         public float maxhealth;
-        //private UnityEngine.AI.NavMeshAgent navLook = null;
-        //private Animator navAnim = null;
-        //public GameObject playerWhere = null;
+        public Animator bugDeath;
         public Transform bugStartPoint;
-        //public Transform bugEndpoint;
-        //private bool bugAttention = false;        
-        //Vector3 heWent;
-        //Quaternion thataWay;
-        /*
-        [SerializeField]
-        private float navWaitTime = 10f;
-        private float navWaitTimer = Mathf.Infinity;
-        */
+        
         public GameObject lightPost;
         public BugEnemy otherScript;
         public bugFirstMoves otherOtherScript;
@@ -28,14 +18,8 @@ namespace YenensTale {
         
         void Start() {
             currentHealth = maxhealth;
-            //navLook = GetComponent<UnityEngine.AI.NavMeshAgent>();
-            //navAnim = GetComponent<Animator>();
-            //navLook.acceleration = 0;
-
-            //===========you might need to add the below line back in=================
             transform.position = bugStartPoint.position;
-            //otherScript = GetComponent<BugEnemy>();
-            //otherOtherScript = GetComponent<bugFirstMoves>();
+            
         }
 
         public void PerformAttack() {
@@ -49,34 +33,17 @@ namespace YenensTale {
         }
 
         void Die() {
-            Destroy(gameObject);
+            //Destroy(gameObject);
+            bugDeath.SetTrigger("Bug Dead");
         }
 
         void FixedUpdate()
         {
-            //navWaitTimer -= Time.deltaTime;
-            /*if (bugAttention)
-            {
-                
-                navLook.acceleration = 5;                
-                navLook.SetDestination(bugEndpoint.transform.position);
-                //float distance = Vector3.Distance(playerWhere.transform.position, transform.position);
-                //heWent was previously direction
-                heWent = (bugEndpoint.transform.position - transform.position).normalized;
-                //Bug turns and looks at (player's direction)
-                thataWay = Quaternion.LookRotation(heWent);
-                transform.rotation = Quaternion.Slerp(transform.rotation, thataWay, Time.deltaTime * 5.0f);
-                //startCount();  
-                
-                //navWaitTimer = navWaitTime;
-                
-
-            }*/
+            
 
             if (Vector3.Distance(transform.position, lightPost.transform.position) <= 20)
             {
-                //bugAttention = false;
-                //navLook.acceleration = 0;
+                
                 otherOtherScript.enabled = false;
                 //deactivate and reactivate the navmesh
                 navTesting.enabled = false;
@@ -84,47 +51,18 @@ namespace YenensTale {
                 otherScript.enabled = true;
             }
 
-            /*if (navWaitTimer <= 0)
-            {
-                //bugAttention = false;
-                //BugCombat();
-
-
-
-
-                navWaitTimer = Mathf.Infinity;
-            }*/
+            
         }
 
         
 
-        /*public void BugCombat()
-        {
-            navLook.acceleration = 0;
-            navLook.SetDestination(playerWhere.transform.position);
-            //float distance = Vector3.Distance(playerWhere.transform.position, transform.position);
-            //heWent was previously direction
-            heWent = (playerWhere.transform.position - transform.position).normalized;
-            //Bug turns and looks at (player's direction)
-            thataWay = Quaternion.LookRotation(heWent);
-            transform.rotation = Quaternion.Slerp(transform.rotation, thataWay, Time.deltaTime * 5.0f);
-        }*/
-        /*
-        public void startCount()
-        {
-            navWaitTimer = navWaitTime;
-        }*/
+        
 
         public void BugMove()
         {
-            //bugAttention = true;
+            
             otherOtherScript.enabled = true;
-            //navLook.acceleration = 5;
-            //transform.position = Vector3.Lerp(bugStartPoint.position, bugEndpoint.position, 10f * Time.deltaTime);
-            /*
-            targetPosition += raiseBy[level++];
-            lerpTimer = 0;
-            */
+            
         }
     }
 }
