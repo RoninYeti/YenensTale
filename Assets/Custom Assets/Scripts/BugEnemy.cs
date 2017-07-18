a7f150291f781557ebd5b4f772fb76c275a63b6b
agleverone <alexgreglev@gmail.com>
2017-07-18 04:34:57 -0400

    stuff i guess

--- Assets/Custom Assets/Scripts/BugEnemy.cs
+++ Assets/Custom Assets/Scripts/BugEnemy.cs
@@ -6,10 +6,14 @@ namespace YenensTale {
     public class BugEnemy : MonoBehaviour { 
     
         private UnityEngine.AI.NavMeshAgent navLook = null;
-        //private Animator navigAnimation = null;
+        public Animator attackAnim;
         public GameObject playerLoc = null;
         Vector3 heWent;
         Quaternion thataWay;
+        public float attackTime = 5;
+        private float attackingTimer = 0;
+        //the below float was used to simply show us usable parameters in the inspector
+        //public float distdisplay = 0;
 
         void Start() {
             navLook = GetComponent<UnityEngine.AI.NavMeshAgent>();
@@ -17,14 +21,28 @@ namespace YenensTale {
         }
 
         void FixedUpdate() {
+            attackingTimer += Time.deltaTime;
             navLook.acceleration = 0;
             navLook.SetDestination(playerLoc.transform.position);
-            //float distance = Vector3.Distance(playerWhere.transform.position, transform.position);
+            float distance = Vector3.Distance(playerLoc.transform.position, transform.position);
             //heWent was previously direction
             heWent = (playerLoc.transform.position - transform.position).normalized;
             //Bug turns and looks at (player's direction)
             thataWay = Quaternion.LookRotation(heWent);
             transform.rotation = Quaternion.Slerp(transform.rotation, thataWay, Time.deltaTime * 5.0f);
+            
+            if (attackingTimer >= attackTime && distance <= 25)
+            {
+                attack();
+                attackingTimer = 0;
+            }
+            //distdisplay = distance;
+        }
+
+        public void attack()
+        {
+            //add lightning stuff here
+            attackAnim.SetTrigger("Bug Attack");
         }
     }
 }
--- Assets/Custom Assets/Scripts/NormgBug.cs
+++ Assets/Custom Assets/Scripts/NormgBug.cs
@@ -9,11 +9,11 @@ namespace YenensTale {
         public float maxhealth;
         public Animator bugDeath;
         //public Transform bugStartPoint;
-        public GameObject lightPost;
+        public GameObject bugPlace;
         public BugEnemy otherScript;
         public bugFirstMoves otherOtherScript;
         public UnityEngine.AI.NavMeshAgent navTesting = null;
-        
+                
         void Start() {
             currentHealth = maxhealth;
             //transform.position = bugStartPoint.position;  
@@ -32,10 +32,11 @@ namespace YenensTale {
         void Die() {
             //Destroy(gameObject);
             bugDeath.SetTrigger("Bug Dead");
+            otherScript.enabled = false;
         }
 
         void FixedUpdate() {
-            if (Vector3.Distance(transform.position, lightPost.transform.position) <= 20) {
+            if (Vector3.Distance(transform.position, bugPlace.transform.position) <= 20) {
                 otherOtherScript.enabled = false;
                 //deactivate and reactivate the navmesh
                 navTesting.enabled = false;
